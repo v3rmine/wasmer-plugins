456f49bac9f4a5ac554a5fd08c555ae2b976663c
joxcat <dev@@planchon.xyz>
2022-03-26 20:26:16 +0000

    [WIP] feat: cleanup

--- examples/host-func-runner/src/main.rs
+++ examples/host-func-runner/src/main.rs
@@ -4,13 +4,10 @@
 use std::{env, process};
 
 use host_func_abi::GetVersionReq;
-use semver::{Version, VersionReq};
 use wasm_plugins::runtime::{get_func_complex_without_args, InstanceBuilder};
-use wasmer::{imports, Function, Store};
+use wasmer::{imports, Function, Memory, Store};
 
 type StdResult<T> = Result<T, Box<dyn std::error::Error>>;
-
-const RUNNER_VERSION: &str = "0.1.0";
 
 fn main() {
     let mut args = env::args();
@@ -27,23 +24,18 @@ fn main() {
     };
 }
 
-fn get_page(url: String) -> String {
-    println!("Fetching {}", url);
-    "It work".to_string()
-}
-
 fn run_plugin(path: &str) -> StdResult<()> {
     let store = Store::default();
-    let get_page = Function::new_native(&store, |args: i32| {
-        println!("IT WORK {}", args);
-        args - 1
+    let get_page = Function::new_native(&store, |ptr: i32, len: i32| {
+        println!("IT WORK {}", ptr);
+        len - 1
     });
 
     let instance = InstanceBuilder::builder()
         .wasm_path(path)
         .import_object(imports! {
             "env" => {
-                "get_page"  => get_page,
+                "_get_page"  => get_page,
             }
         })
         .build()
@@ -54,18 +46,7 @@ fn run_plugin(path: &str) -> StdResult<()> {
     let get_plugin_requirements = get_func_complex_without_args::<GetVersionReq>(&instance)?;
 
     let plugin_requirements = get_plugin_requirements()?;
-
-    println!("Runner version is {}", RUNNER_VERSION);
     println!("Plugin requirements are {}", plugin_requirements);
-
-    let semver_req = VersionReq::parse(&plugin_requirements)?;
-    let runner_version = Version::parse(RUNNER_VERSION)?;
-
-    if semver_req.matches(&runner_version) {
-        println!("The runner support this plugin");
-    } else {
-        println!("The runner does not support this plugin");
-    }
 
     Ok(())
 }
